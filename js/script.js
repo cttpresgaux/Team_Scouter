a09434c81606b664f3c6cc6006fe17aaf3f5adf5
Emeric Beauchot <Ricco7La@users.noreply.github.com>
2022-09-21 22:56:56 +0200

    Update script.js
    change category number

--- js/script.js
+++ js/script.js
@@ -195,7 +195,7 @@ function valider() {
             +       '<tab:GetMatchesRequest>'
             +           '<tab:Club>' + adv.club + '</tab:Club>'
             +           '<tab:Team>' + letter + '</tab:Team>'
-    	    //+		    '<tab:DivisionCategory>1</tab:DivisionCategory>'
+    	    +		    '<tab:DivisionCategory>37</tab:DivisionCategory>'
             +           '<tab:WithDetails>Yes</tab:WithDetails>'
             +       '</tab:GetMatchesRequest>'
             +   '</soapenv:Body>'
